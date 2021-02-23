c46b8d33db15f938322275bf2f7d36960a2d397a
rgederin <gederin.ruslan@gmail.com>
2021-02-23 22:42:40 +0200

    add x ray

--- event-driven-serverless-application/services/s3-sqs-service/src/utils/aws.js
+++ event-driven-serverless-application/services/s3-sqs-service/src/utils/aws.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 AWS.config.region = process.env.REGION;
 
--- event-driven-serverless-application/services/sqs-dynamodb-service/src/utils/aws.js
+++ event-driven-serverless-application/services/sqs-dynamodb-service/src/utils/aws.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 const client = new AWS.DynamoDB.DocumentClient();
 
